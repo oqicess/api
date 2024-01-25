ac6e2407af1c3be355f5aa93688da1214d0e8255
This4User <danilagamelol@gmail.com>
2024-01-25 23:00:30 +0300

    fix: cors

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,18 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import {NestFactory} from '@nestjs/core';
+import {AppModule} from './app.module';
 import database from './database';
 import config from './config';
 
 async function start() {
 
-  const app = await NestFactory.create(AppModule);
+    const app = await NestFactory.create(AppModule, {cors: true});
 
-  database.connect()
-    .then(() => {
-        app.listen(config.app.port, () => {
-            console.log(`Server listening on port ${config.app.port}`);
+    database.connect()
+        .then(() => {
+            app.listen(config.app.port, () => {
+                console.log(`Server listening on port ${config.app.port}`);
+            });
         });
-    });
 }
 
 start();
